20d0b8640d5e7ea62bb329f8d4a15f6b6a8bd657
Özenç Bilgili <bilgiliozenc@gmail.com>
2020-06-05 16:38:55 +0300

    Improve key default prevention

--- Projects/JS Keycodes/js/script.js
+++ Projects/JS Keycodes/js/script.js
@@ -1,29 +1,4 @@
 window.addEventListener('keydown', (e) => {
-	// Prevent backspace key from navigating back in browser history
-	let doPrevent = false;
-    if (e.keyCode === 8) {
-        let d = e.srcElement || e.target;
-        if ((d.tagName.toUpperCase() === 'INPUT' && 
-             (
-                 d.type.toUpperCase() === 'TEXT' ||
-                 d.type.toUpperCase() === 'PASSWORD' || 
-                 d.type.toUpperCase() === 'FILE' || 
-                 d.type.toUpperCase() === 'SEARCH' || 
-                 d.type.toUpperCase() === 'EMAIL' || 
-                 d.type.toUpperCase() === 'NUMBER' || 
-                 d.type.toUpperCase() === 'DATE' )
-             ) || 
-             d.tagName.toUpperCase() === 'TEXTAREA') {
-            doPrevent = d.readOnly || d.disabled;
-        }
-        else {
-            doPrevent = true;
-        }
-    }
-
-    if (doPrevent) {
-        e.preventDefault();
-    }
-
+	e.preventDefault();
 	document.querySelector('#keycode').innerText = e.keyCode;
 });
